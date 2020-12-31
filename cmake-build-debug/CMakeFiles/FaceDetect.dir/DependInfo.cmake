a4a555ecd50929397fa677a896a464d3c89c4fad
mactty <mactty@com>
2020-12-31 10:36:59 +0800

    first commit

---
+++ cmake-build-debug/CMakeFiles/FaceDetect.dir/DependInfo.cmake
@@ -0,0 +1,38 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/mactty/project/opencv/FaceDetect/main.cpp" "/Users/mactty/project/opencv/FaceDetect/cmake-build-debug/CMakeFiles/FaceDetect.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/Users/mactty/project/opencv/opencv-4.5.1/build"
+  "/Users/mactty/project/opencv/opencv-4.5.1/include"
+  "/Users/mactty/project/opencv/opencv-4.5.1/modules/core/include"
+  "/Users/mactty/project/opencv/opencv-4.5.1/modules/flann/include"
+  "/Users/mactty/project/opencv/opencv-4.5.1/modules/imgproc/include"
+  "/Users/mactty/project/opencv/opencv-4.5.1/modules/ml/include"
+  "/Users/mactty/project/opencv/opencv-4.5.1/modules/photo/include"
+  "/Users/mactty/project/opencv/opencv-4.5.1/modules/dnn/include"
+  "/Users/mactty/project/opencv/opencv-4.5.1/modules/features2d/include"
+  "/Users/mactty/project/opencv/opencv-4.5.1/modules/imgcodecs/include"
+  "/Users/mactty/project/opencv/opencv-4.5.1/modules/videoio/include"
+  "/Users/mactty/project/opencv/opencv-4.5.1/modules/calib3d/include"
+  "/Users/mactty/project/opencv/opencv-4.5.1/modules/highgui/include"
+  "/Users/mactty/project/opencv/opencv-4.5.1/modules/objdetect/include"
+  "/Users/mactty/project/opencv/opencv-4.5.1/modules/stitching/include"
+  "/Users/mactty/project/opencv/opencv-4.5.1/modules/ts/include"
+  "/Users/mactty/project/opencv/opencv-4.5.1/modules/video/include"
+  "/Users/mactty/project/opencv/opencv-4.5.1/modules/gapi/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/FaceDetect.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FaceDetect.dir/main.cpp.o"
+  "FaceDetect"
+  "FaceDetect.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FaceDetect.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
